1fc58b85f06539d034dfa9460552ada43d1f975b
Justin Mark <jwmark212@gmail.com>
2021-10-22 07:39:11 -0700

    first commit

---
+++ src/components/About/about.jsx
@@ -0,0 +1,89 @@
+import "./about.css"
+
+
+export default function About() {
+    return (
+        <div>
+
+<div className="container-fluid section1 pt-3 px-sm-5">
+        <div className="row">
+            <div className="col-12 d-flex justify-content-between">
+                <div className="my-auto text-warning">
+                  <img className="img-fluid logo" src="https://res.cloudinary.com/not-set/image/upload/v1634900477/Futurelabs-logo_1_szyxc8.png" alt="" />
+                </div>
+                <div className="d-flex justify-content-around text-white">
+                  <div className="my-auto mobile-no"><h6>All categories</h6></div>
+                  <div className="my-auto mobile-yes"><h5 className="text-white">&#x2970;</h5></div>
+                  <hr className="bg-white mx-4 my-auto horizontal" />
+                  <div className="my-auto">
+                    <div className="menu bg-white"></div>
+                    <div className="menu bg-white"></div>
+                    <div className="menu bg-white"></div>
+                  </div>
+                </div>
+            </div>
+        </div>
+
+        
+        <div className="row d-flex align-content-bottom text-white align-bottom line1">
+          <div className="col-sm-7">
+
+            <h4>WE ARE FUTURISTS!</h4>
+            
+            
+            <h5>
+              We are a dedicated team of creative 
+              <br />
+              strategists, Designers & Developers.
+            </h5>
+
+            <br />
+            
+            <h6>
+              We will help you turn ideas into 
+              the reality you hope to create..
+            </h6>
+          </div>
+
+          <div className="col-sm-5 d-flex justify-content-end pt-5">
+
+            <img className="img-fluid mute mt-5" src="https://res.cloudinary.com/not-set/image/upload/v1634901154/mute_1_2_joyf7a.png" alt="" />
+
+            
+          </div>
+        </div>
+    </div>
+
+
+
+    <div className="container-fluid section2">
+      <div className="row">
+        <div className="col-md-6 text-black pl-sm-5 py-5">
+          <h3>
+            We enable you to
+            <br />
+            see beyond the now.
+            <br />
+            You could either be
+            <br />
+            android or you go 
+            <br />
+            extinct like
+            <br />
+            Blackberry...” 
+
+          </h3>
+          <br />
+
+          <p>WE ARE FUTURISTS!</p>
+        </div>
+
+        <div className="col-md-6 bg-warning">
+          jgjgj
+        </div>
+      </div>
+    </div>
+
+    </div>
+    )
+}

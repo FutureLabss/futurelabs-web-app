8c74cd7cdce8f7cd90a14ba581a0e9ffc25d83a6
Justin Mark <jwmark212@gmail.com>
2021-11-05 04:40:19 -0700

    Routing

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -278,60 +278,6 @@ return (
   </div>
 </div>
 
-<div className="container">
-  <div className="row mt-5 d-flex justify-content-around last">
-    <div className="col-sm-3">
-      <h6>
-        <strong>About Future Labs</strong> 
-      </h6>
-
-      <p>
-        We are a dedicated team of creative and innovative content and corporate strategists, Designers, Developers and Leaders;we will help
-        you build your project ideas into the reality you
-        hope to create.
-      </p>
-    </div>
-
-    <div className="col-sm-3">
-      <h6>
-        <strong>Contact Us </strong>
-      </h6>
-
-      <p>
-        +160726007432 <br />
-        hello@futurelabs.agency
-      </p>
-    </div>
-
-    <div className="col-sm-3">
-      <h6>
-        <strong>Let's Get in Touch</strong>
-        <p>
-          Sign up to recieve our newsletter about industry trends
-        </p>
-        <div class="input-group mb-3">
-          <input type="text" class="form-control rounded" placeholder="Enter your@email.com" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-          <div class="input-group-append">
-            <button class="btn btn-outline-secondary text-white email rounded" type="button">Send</button>
-          </div>
-        </div>
-
-      </h6>
-
-    </div>
-
-
-  </div>
-</div>
-
-<div className="container-fluid">
-  <div className="row">
-    <div className="col-12 foot">
-
-    </div>
-  </div>
-</div>
-
 
 
 {/* <div className="container-fluid section2 bg-danger">
--- src/index.js
+++ src/index.js
@@ -2,13 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import About from './components/About/about';
+import Home from './components/Home/home';
+import pageRoutes from './router/routes'
+import {
+  BrowserRouter ,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import Footer from './components/layout/footer';
 // import './index.css';
 // import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(
-  <About />,
+  <BrowserRouter>
+    <Routes>
+      {
+        pageRoutes.map((item)=>{
+          return <Route path={item.path} element={<item.element />} />
+        })
+      }
+    </Routes>
+    <Footer />
+  </BrowserRouter>,
   document.getElementById('root')
 );
 

76294ee40ae578507cdd62a501e1fa621e6a6e22
BryanEtim <bryanetim@gmail.com>
2021-11-16 17:43:13 +0100

    new

--- src/components/layout/toolbar.js
+++ src/components/layout/toolbar.js
@@ -52,6 +52,7 @@ export default function ToolBar(props){
          <Link to="/about"> ABOUT</Link>
          <Link to="/strategy"> STRATEGY</Link>
          <Link to="/modelling"> 3D-MODELLING</Link>
+         <Link to="/products">PRODUCTS</Link>
            <a href="https://futurelabs-agency.netlify.app/">bLOG</a>
          
         </Nav>
--- src/router/routes.js
+++ src/router/routes.js
@@ -3,12 +3,14 @@ import Home from '../components/Home/home'
 import Services from '../components/Services/services'
 import strategy from '../components/strategy/strategy'
 import modelling from '../components/modelling/modelling'
+import product from '../components/Products/product'
 
 const pageRoutes = [
     {path: '/about', element: About },
     {path: '/services', element: Services },
     {path: '/strategy', element: strategy },
     {path: '/modelling', element: modelling },
+    {path: '/products', element: product},
     {path: '/', element: Home },
 ]
 

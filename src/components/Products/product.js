46d866572873512ebff9830b530a1b51848318a1
BryanEtim <bryanetim@gmail.com>
2021-11-18 11:18:57 +0100

    dogs

--- src/components/Products/product.js
+++ src/components/Products/product.js
@@ -7,14 +7,14 @@ export default function product() {
            <ToolBar />
            <section>
            <div className="pictur200">
-           <div className="punchbox2 text-white container">
+           <div className="punchbox270 text-white container">
 
-<h4 className="punchlines bottom-left veers">
+<h4 className="punchlines340 bottom-left veers300">
 Product Design
 
 </h4>
 
-<h5 className="sub-lines ">
+<h5 className="sub-lines140 ">
 World class digital products
 <br/>that accelerate profit
 </h5>
@@ -38,13 +38,13 @@ World class digital products
            </section>
            <section>
                <div className="picturet200">
-               <div className="punchbox2 text-white container">
+               <div className="punchbox270 text-white container">
 
-<h4 className="punchlines2 veer">
+<h4 className="punchlines250 veer900">
 Access Your Computer <br/>Science Curriculum on the Go 
 </h4>
 
-<h5 className="sub-lines2">
+<h5 className="sub-lines250">
 Re-defining The Science Of Computers
 </h5>
 
@@ -57,13 +57,13 @@ Re-defining The Science Of Computers
            </section>
            <section>
                <div className="pictures500">
-               <div className="punchbox2 text-white container " >
+               <div className="punchbox220 text-white container " >
 
-<h4 className="punchlines veer">
+<h4 className="punchlines400 veer150">
 How About A Seamless<br/> Driving Experience?
 </h4>
 
-<h5 className="sub-lines">
+<h5 className="sub-lines300">
 We partnered with Zuga to deliver <br/>effortless transportation
 </h5>
 
@@ -76,13 +76,13 @@ We partnered with Zuga to deliver <br/>effortless transportation
            </section>
            <section>
            <div className="picture600">
-           <div className="punchbox2 text-white container">
+           <div className="punchbox2000 text-white container">
 
-<h4 className="punchlines veers">
+<h4 className="punchlines200 veers20">
 Seamless Cross-border<br/> Payments and Currency<br/>Conversion
 </h4>
 
-<h5 className="sub-lines">
+<h5 className="sub-lines200">
 Securing Payments for Tomorrow
 </h5>
 

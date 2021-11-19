166d69e87de73e24cbfe38cb295d0f6437f7a3d9
BryanEtim <bryanetim@gmail.com>
2021-11-19 16:03:15 +0100

    cats

--- src/components/Products/product.js
+++ src/components/Products/product.js
@@ -36,16 +36,34 @@ World class digital products
 
 </div>
            </section>
-          
+           <section>
+               <div className="pictures501">
+               <div className="punchbox270 text-white container " >
+
+<h4 className="punchlines340 veer150">
+Access Your Computer<br/>Science Curriculum on the Go
+</h4>
+
+<h5 className="sub-lines140">
+Re-defining The Science Of Computers
+</h5>
+
+<br />
+  
+
+
+</div>
+               </div>
+           </section>
            <section>
                <div className="pictures500">
-               <div className="punchbox220 text-white container " >
+               <div className="punchbox270 text-white container " >
 
-<h4 className="punchlines400 veer150">
+<h4 className="punchlines340 veer150">
 How About A Seamless<br/> Driving Experience?
 </h4>
 
-<h5 className="sub-lines300">
+<h5 className="sub-lines140">
 We partnered with Zuga to deliver <br/>effortless transportation
 </h5>
 
@@ -58,13 +76,13 @@ We partnered with Zuga to deliver <br/>effortless transportation
            </section>
            <section>
            <div className="picture600">
-           <div className="punchbox2000 text-white container">
+           <div className="punchbox270 text-white container">
 
-<h4 className="punchlines200 veers20">
+<h4 className="punchlines340 veers20">
 Seamless Cross-border<br/> Payments and Currency<br/>Conversion
 </h4>
 
-<h5 className="sub-lines200">
+<h5 className="sub-lines140">
 Securing Payments for Tomorrow
 </h5>
 

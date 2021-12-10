aea55cab5baf7ecbb9353876843bc2a8b00490f5
Justin Mark <jwmark212@gmail.com>
2021-12-10 03:43:59 -0800

    updated code

--- src/Blog/Homepage.js
+++ src/Blog/Homepage.js
@@ -145,7 +145,7 @@ export default function PersistentDrawerRight() {
       >
         <Toolbar>
           <Typography variant="h6" noWrap className={classes.title}>
-            Future Labs
+            FutureLabs
           </Typography>
           <IconButton
             color="inherit"
@@ -193,7 +193,7 @@ export default function PersistentDrawerRight() {
           </IconButton>
         </div>
         <Divider />
-        <Nav.Link href="/"><img src="https://res.cloudinary.com/not-set/image/upload/v1634900477/Futurelabs-logo_1_szyxc8.png" alt=""/></Nav.Link>
+        <Nav.Link href="https://futurelabs-agency1.netlify.app/"><img src="https://res.cloudinary.com/not-set/image/upload/v1634900477/Futurelabs-logo_1_szyxc8.png" alt=""/></Nav.Link>
       </Drawer>
 
       
--- src/index.js
+++ src/index.js
@@ -3,16 +3,6 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 //import About from './components/About/about';
 //import Home from './components/Home/home';
-import pageRoutes from './router/routes'
-import {
-  BrowserRouter ,
-  Routes,
-  Route,
-//  Link
-} from "react-router-dom";
-import Footer from './components/layout/footer';
-import ToolBar from './components/layout/toolbar';
-import Contact from './components/layout/contact';
 import Blog from './Blog/Homepage'
 // import './index.css';
 // import App from './App';

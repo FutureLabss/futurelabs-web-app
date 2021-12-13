7d9d74167ece14bb1bb82f6ec7b5e2f9e176c35a
Usenmfon <usenmfonuko@gmail.com>
2021-12-13 12:16:19 +0100

    changes

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,278 +1,289 @@
-import "./home.css"
-import ToolBar from "../layout/toolbar"
-
+import "./home.css";
+import ToolBar from "../layout/toolbar";
 
 export default function Home() {
-return (
-      
-<div>
+  return (
+    <div>
+      <ToolBar />
 
-  <ToolBar />                                           
-          
-<div className="section1">
+      <div className="section1">
+        <div className="punchbox text-white">
+          <h4 className="punchline">WE ARE FUTURISTS!</h4>
 
-  <div className="punchbox text-white">
+          <h5 className="sub-line">
+            We create indelible <br />
+            digital experiences.
+          </h5>
 
-    <h4 className="punchline">
-      WE ARE FUTURISTS!
-    </h4>
+          <br />
+        </div>
 
-    <h5 className="sub-line">
-    We create indelible <br/>
-digital experiences.      
-    </h5>
-
-    <br />
-      
-   
-
-  </div>
-
-  <img className="img-fluid mute " src="https://res.cloudinary.com/not-set/image/upload/v1634901154/mute_1_2_joyf7a.png" alt="" />
-
-    
-</div>
-
-
-
-{/* Section 2 begins here */}
-<div className="container-fluid section2">
-  <div className="row">
-
-    <div className="col-sm-6 section2-box1">    
-    
-      <div className="section2-box1-inner">            
-      <h5 className="fontys mb-4"> We are  Future Labs!</h5>
-        <h3 className="textss">
-        Futurelabs is a digital<br/> agency with global <br/>footprints.
-        </h3>
-        <br />
-        <h5 className="fonty2">Enhancing User Experience and turning<br/> ideas to realities one elated client a time </h5>
-    <button  className="but mb-5 mt-3">learn more</button>
+        <img
+          className="img-fluid mute "
+          src="https://res.cloudinary.com/not-set/image/upload/v1634901154/mute_1_2_joyf7a.png"
+          alt=""
+        />
       </div>
-        
-    </div>
 
-    <div className="col-sm-6 p-0 section2-box2 text-center">
-      <div className="image2 text-center ">
-      <img className="piks" src="https://res.cloudinary.com/dekbvdqnb/image/upload/v1635262165/Futurelabs-logo_2_fsvqs9.png" alt="" />
-       
-        
-        <div className="section2-mobile-yes">
-          <h3 className="section2-mobile-inner"> 
-          <h5 className="fontys"> We are  Future Labs!</h5>
-        <h3 className=". ">
-        Futurelabs is a digital<br/> agency with global <br/>footprints.
-        </h3>
-        <br />
-        <h5 className="fonty2 text-white">Enhancing User Experience<br/> and turning ideas to realities<br/> one elated client a time </h5>
-    <button  className="but mt-3">learn more</button>
-          </h3>
-          
+      {/* Section 2 begins here */}
+      <div className="container-fluid section2">
+        <div className="row">
+          <div className="col-sm-6 section2-box1">
+            <div className="section2-box1-inner">
+              <h5 className="fontys mb-4"> We are Future Labs!</h5>
+              <h3 className="textss">
+                Futurelabs is a digital
+                <br /> agency with global <br />
+                footprints.
+              </h3>
+              <br />
+              <h5 className="fonty2">
+                Enhancing User Experience and turning
+                <br /> ideas to realities one elated client a time{" "}
+              </h5>
+              <button className="but mb-5 mt-3">learn more</button>
+            </div>
+          </div>
+
+          <div className="col-sm-6 p-0 section2-box2 text-center">
+            <div className="image2 text-center ">
+              <img
+                className="piks"
+                src="https://res.cloudinary.com/dekbvdqnb/image/upload/v1635262165/Futurelabs-logo_2_fsvqs9.png"
+                alt=""
+              />
+
+              <div className="section2-mobile-yes">
+                <h3 className="section2-mobile-inner">
+                  <h5 className="fontys"> We are Future Labs!</h5>
+                  <h3 className=". ">
+                    Futurelabs is a digital
+                    <br /> agency with global <br />
+                    footprints.
+                  </h3>
+                  <br />
+                  <h5 className="fonty2 text-white">
+                    Enhancing User Experience
+                    <br /> and turning ideas to realities
+                    <br /> one elated client a time{" "}
+                  </h5>
+                  <button className="but mt-3">learn more</button>
+                </h3>
+
+                {/* <br /> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Section 3 begins here */}
+      <div className="container-fluid section2">
+        <div className="row">
+          <div className="col-sm-6 p-0 .section2-box2">
+            <div className="image3 bg-danger">
+              <div className="section3-mobile-yes">
+                <h5 className="fontys"> We tell stories</h5>
+                <h3 className=".">
+                  We craft user-centric <br />
+                  stories about products
+                  <br /> and services that our
+                  <br /> target audience finds
+                  <br /> relatable and actionable.
+                </h3>
+                <button className="but mt-3">learn more</button>
+                {/* <br /> */}
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm-6 section2-box1">
+            <div className="section2-box1-inner">
+              <h5 className="fontys mb-3"> We tell stories</h5>
+              <h3 className="textss">
+                We craft user-centric <br />
+                stories about products
+                <br /> and services that our
+                <br /> target audience finds
+                <br /> relatable and actionable.
+              </h3>
+              <button className="but mt-3">learn more</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="section4">
+        <div className="punchbox42 text-white">
+          <h5 className="sub-line subline2">
+            We Strategize <br className="no-break-mobile" />
+            We take our time <br className="no-break-mobile" />
+            sifting through Data <br className="no-break-mobile" />
+            and building models
+          </h5>
+          <br />
+          <br />
+
           {/* <br /> */}
-       
-       
-        
+          <img
+            className="img-fluid mute123"
+            src="https://res.cloudinary.com/not-set/image/upload/v1634901154/mute_1_2_joyf7a.png"
+            alt=""
+          />
+        </div>
       </div>
-      </div>
-      
-    </div>
-  </div>
-</div>
 
-
-
-
-
-{/* Section 3 begins here */}
-<div className="container-fluid section2">
-  <div className="row">
-    <div className="col-sm-6 p-0 .section2-box2">
-      
-      <div className="image3 bg-danger">
-       
-        
-        <div className="section3-mobile-yes">
-        <h5 className="fontys"> We tell stories</h5>
-        <h3 className=".">
-        We craft user-centric <br/>stories about products<br/> and services that our<br/> target audience finds<br/> relatable and actionable.
-        
-        </h3>
-        <button  className="but mt-3">learn more</button>
-        {/* <br /> */}
-       
-       
-        
-      </div>
-      </div>
-      
-    </div>
-
-
-    <div className="col-sm-6 section2-box1">
-
-      <div className="section2-box1-inner">
-      <h5 className="fontys mb-3"> We tell stories</h5>
-        <h3 className="textss">
-        We craft user-centric <br/>stories about products<br/> and services that our<br/> target audience finds<br/> relatable and actionable.
-        
-        </h3>
-        <button  className="but mt-3">learn more</button>
-      </div>
-      
-    </div>
-  </div>
-</div>
-
-
-
-
-<div className="section4">
-
-  <div className="punchbox42 text-white">
-
-    <h5 className="sub-line subline2">
-    We Strategize <br className="no-break-mobile" />
-    We take our time <br className="no-break-mobile" />
-    sifting through Data <br className="no-break-mobile" />
-    and building models
-    </h5>
-    <br />
-    <br />
-    
-    {/* <br /> */}
-    <img className="img-fluid mute123" src="https://res.cloudinary.com/not-set/image/upload/v1634901154/mute_1_2_joyf7a.png" alt="" />
-        
-
-  </div>
-
- 
-
-    
-</div>
-
-<div className="section45">
-
- 
-   
-
-    
-</div>
-<div className="container sec">
+      <div className="section45"></div>
+      <div className="container sec">
         <div className="row">
           <div className="col-md-4 mt-5">
-        <div className="card">
-          <img className="card-img-top h" src="https://res.cloudinary.com/dekbvdqnb/image/upload/v1635348411/priscilla-du-preez-XkKCui44iM0-unsplash_1_pd3v8t.png" alt="" srcset=""/>
-          <div className="card-body">
-            <h5 className="card-tittle var">The coca-cola Strategy</h5>
-            <p className="p card-text">29 sep , 2021 </p>
-            <div className="sli"></div>
-              <p className="card-text vars mt-3"> Coca-cola focuses on improving the community relationships and
-               increasing thhier happiness,that positively reflects on thier public image, resulting
-               in customer and revenue rise. </p>
-                
-            
-          </div>
-        </div>
-          </div>
-          <div className="col-md-4 mt-5">
-          <div className="card">
-          <img className="card-img-top h" src="https://res.cloudinary.com/dekbvdqnb/image/upload/v1635349111/ben-white-4K2lIP0zc_k-unsplash_1_lew38n.png" alt="" srcset=""/>
-          <div className="card-body">
-            <h5 className="card-tittle var">The coca-cola Strategy</h5>
-            <p className="p card-text">29 sep , 2021 </p>
-            <div className="sli"></div>
-              <p className="card-text vars mt-3"> Coca-cola focuses on improving the community relationships and
-               increasing thhier happiness,that positively reflects on thier public image, resulting
-               in customer and revenue rise. </p>
-                
-            
-          </div>
-        </div>
+            <div className="card">
+              <img
+                className="card-img-top h"
+                src="https://res.cloudinary.com/dekbvdqnb/image/upload/v1635348411/priscilla-du-preez-XkKCui44iM0-unsplash_1_pd3v8t.png"
+                alt=""
+                srcset=""
+              />
+              <div className="card-body">
+                <h5 className="card-tittle var">The coca-cola Strategy</h5>
+                <p className="p card-text">29 sep , 2021 </p>
+                <div className="sli"></div>
+                <p className="card-text vars mt-3">
+                  {" "}
+                  Coca-cola focuses on improving the community relationships and
+                  increasing thhier happiness,that positively reflects on thier
+                  public image, resulting in customer and revenue rise.{" "}
+                </p>
+              </div>
+            </div>
           </div>
           <div className="col-md-4 mt-5">
-          <div className="card">
-          <img className="card-img-top h" src="https://res.cloudinary.com/dekbvdqnb/image/upload/v1635349190/scott-graham-5fNmWej4tAA-unsplash_1_mcymci.png" alt="" srcset=""/>
-          <div className="card-body">
-            <h5 className="card-tittle var">The coca-cola Strategy</h5>
-            <p className="p card-text">29 sep , 2021 </p>
-            <div className="sli"></div>
-              <p className="card-text vars mt-3">  Coca-cola focuses on improving the community relationships and
-               increasing<br/> thhier happiness,that positively reflects <br/>on thier public image, resulting<br/>
-               in customer and revenue rise.</p> 
-                
-            
+            <div className="card">
+              <img
+                className="card-img-top h"
+                src="https://res.cloudinary.com/dekbvdqnb/image/upload/v1635349111/ben-white-4K2lIP0zc_k-unsplash_1_lew38n.png"
+                alt=""
+                srcset=""
+              />
+              <div className="card-body">
+                <h5 className="card-tittle var">The coca-cola Strategy</h5>
+                <p className="p card-text">29 sep , 2021 </p>
+                <div className="sli"></div>
+                <p className="card-text vars mt-3">
+                  {" "}
+                  Coca-cola focuses on improving the community relationships and
+                  increasing thhier happiness,that positively reflects on thier
+                  public image, resulting in customer and revenue rise.{" "}
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 mt-5">
+            <div className="card">
+              <img
+                className="card-img-top h"
+                src="https://res.cloudinary.com/dekbvdqnb/image/upload/v1635349190/scott-graham-5fNmWej4tAA-unsplash_1_mcymci.png"
+                alt=""
+                srcset=""
+              />
+              <div className="card-body">
+                <h5 className="card-tittle var">The coca-cola Strategy</h5>
+                <p className="p card-text">29 sep , 2021 </p>
+                <div className="sli"></div>
+                <p className="card-text vars mt-3">
+                  {" "}
+                  Coca-cola focuses on improving the community relationships and
+                  increasing
+                  <br /> thhier happiness,that positively reflects <br />
+                  on thier public image, resulting
+                  <br />
+                  in customer and revenue rise.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
+      </div>
+      <div className="container mb-5">
+        <div className="row pt-3">
+          <div className="">
+            <h5>
+              <strong className="contact container">Contact us</strong>
+            </h5>
           </div>
         </div>
-        </div>  
-<div className="container mb-5">
-  <div className="row pt-3">
-    <div className="">
-      <h5><strong className="contact container">Contact us</strong></h5>
+        <div className="row pt-3 px-2">
+          <div className="col-sm-6">
+            <div class="input-group mb-2  h-25">
+              <input
+                type="text"
+                class="form-control inputs  rounded"
+                placeholder="Your name"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <div class="input-group-append">
+                {/* <button class="btn btn-outline-secondary" type="button">Button</button> */}
+              </div>
+            </div>
+
+            <div class="input-group mb-3  h-25">
+              <input
+                type="text"
+                class="form-control inputs rounded"
+                placeholder="Your phone number"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <div class="input-group-append">
+                {/* <button class="btn btn-outline-secondary" type="button">Button</button> */}
+              </div>
+            </div>
+
+            <div class="input-group mb-4 h-25">
+              <input
+                type="text"
+                class="form-control inputs rounded"
+                placeholder="Your company"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <div class="input-group-append">
+                {/* <button class="btn btn-outline-secondary" type="button">Button</button> */}
+              </div>
+            </div>
+
+            <div class="input-group mb-5 h-25">
+              <input
+                type="text"
+                class="form-control inputs rounded"
+                placeholder="Your@email.com"
+                aria-label="Recipient's username"
+                aria-describedby="basic-addon2"
+              />
+              <div class="input-group-append">
+                {/* <button class="btn btn-outline-secondary" type="button">Button</button> */}
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm-6">
+            <div class=" message">
+              <textarea
+                class="form-control inputss rounded "
+                placeholder="Leave a comment here"
+                id="floatingTextarea2"
+              ></textarea>
+            </div>
+
+            <div className="d-flex sends ">
+              <button class="mx-auto  submit rounded ">
+                <strong>Submit</strong>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  </div>
-  <div className="row pt-3 px-2">
-    <div className="col-sm-6">
-
-      <div class="input-group mb-2  h-25">
-        <input type="text" class="form-control inputs  rounded" placeholder="Your name" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-        <div class="input-group-append">
-          {/* <button class="btn btn-outline-secondary" type="button">Button</button> */}
-        </div>
-      </div>
-
-      <div class="input-group mb-3  h-25">
-        <input type="text" class="form-control inputs rounded" placeholder="Your phone number" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-        <div class="input-group-append">
-          {/* <button class="btn btn-outline-secondary" type="button">Button</button> */}
-        </div>
-      </div>
-
-      <div class="input-group mb-4 h-25">
-        <input type="text" class="form-control inputs rounded" placeholder="Your company" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-        <div class="input-group-append">
-          {/* <button class="btn btn-outline-secondary" type="button">Button</button> */}
-        </div>
-      </div>
-
-      <div class="input-group mb-5 h-25">
-        <input type="text" class="form-control inputs rounded" placeholder="Your@email.com" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-        <div class="input-group-append">
-          {/* <button class="btn btn-outline-secondary" type="button">Button</button> */}
-        </div>
-      </div>
-
-      
-    </div>  
-
-    <div className="col-sm-6">
-    <div class=" message">
-  <textarea class="form-control inputss rounded " placeholder="Leave a comment here" id="floatingTextarea2" ></textarea>
-                                 
-</div>
-   
-
-      <div className="d-flex sends ">
-        <button class="mx-auto  submit rounded "><strong>Submit</strong></button>
-      </div>
-
-      
-
-      
-    </div>
-
-
-  </div>
-</div>
-
-
-
-
-
-
-
-    </div>
-    )
+  );
 }

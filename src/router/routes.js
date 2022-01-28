63532ea5bed2859a1e4b4456dabb3a7be2052683
Justin Mark <jwmark212@gmail.com>
2022-01-28 06:47:34 -0800

    new changes for dev

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,7 +2,7 @@ import Home from '../components/Home/home'
 
 import About from '../components/About/about'   
 import Services from '../components/Services/services'
-// import Strategy from '../components/Strategy/strategy'
+import Strategy from '../components/Strategy/strategy'
 import Branding from '../components/Branding/branding'
 
 import PersistentDrawerRight from '../Blog/Homepage'
@@ -15,27 +15,17 @@ import DigitalArt from '../components/DigitalArt/digitalArt'
 import singlepost from '../components/singlepost/singlepost'
 
 const pageRoutes = [
+    {path: '/', element: Home },
     {path: '/about', element: About },
     {path: '/services', element: Services },
-    // {path: '/strategy', element: Strategy },
+    {path: '/strategy', element: Strategy },
     {path: '/branding', element: Branding },
-
     {path: '/modelling', element: Modelling },
     {path: '/products', element: Products },
     {path: '/websites', element: Websites },
     {path: '/digitalArt', element: DigitalArt },
     {path: '/singlepost/:id', element: singlepost },
-
-
     {path: '/blog', element: PersistentDrawerRight },
-
-    // {path: '/about', element: About },
-    // {path: '/services', element: Services },
-    // {path: '/strategy', element: strategy },
-    // {path: '/modelling', element: modelling },
-    // {path: '/products', element: product },
-    // {path: '/websites', element: websites },
-    {path: '/', element: Home },
 ]
 
 export default pageRoutes

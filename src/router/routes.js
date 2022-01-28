6c19e6ad26cc06ca5470c47fe1361ccf8e864fe0
Justin Mark <jwmark212@gmail.com>
2022-01-28 07:45:18 -0800

    new changes for dev

--- src/router/routes.js
+++ src/router/routes.js
@@ -11,7 +11,7 @@ import Modelling from '../components/modelling/modelling'
 import Products from '../components/Products/products'
 
 import Websites from '../components/websites/websites'
-import DigitalArt from '../components/digitalArt/digitalart'
+import DigitalArt from '../components/digitalArt/digitalart.js'
 import singlepost from '../components/singlepost/singlepost'
 import PersistentDrawerRight from '../Blog/Homepage'
 

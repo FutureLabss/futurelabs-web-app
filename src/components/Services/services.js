4ee19728ec9038ca15f71fbc3d95492f53d7ccee
Justin Mark <jwmark212@gmail.com>
2022-02-08 06:57:14 -0800

    MODELLING Video uploaded

--- src/components/Services/services.js
+++ src/components/Services/services.js
@@ -152,36 +152,46 @@ return (
     {/* Section-One ends here */}
 
 
+    
 
 
-    {/* Section-One begins here */}
-    <section className="section1 services-image4">
+     {/* Section-One begins here */}
+   <div className="video-container .text-danger .bg-danger">
+        <video autoPlay muted loop className='overall'>
+            {/* <Transformation endOffset="30" videoCodec="auto" /> */}
+            <source 
+              src="https://res.cloudinary.com/not-set/video/upload/v1644321263/Military_Drone_Animation_by_Maverick_for_FutureLABS_jm6vgp.mp4" type="video/mp4" 
+            /> 
+            
+          
+        </video> 
 
-        <div className="container">
-            <div className="row .bg-warning d-flex justify-space-between">
-                <div className="col-9 .bg-primary px-3 px-sm-1 text-white">
+        <div className="parent-box5 d-flex justify-space-between ontop .bg-warning px-5 mx-sm-5 text-white">
+            <div className="text-box .bg-danger">
+                <h5 className="line1">
+                    3D Modelling
+                </h5>
+                    <Link to="/modelling"><button type="button" class="btn btn-outline-warning services-button-text mt-4 text-white py-1 px-5">Learn More</button></Link>
 
-                    <h5 className="line1">
-                        3D Modelling
-                    </h5>
-                    <button type="button" class="btn btn-outline-warning services-button-text mt-4 text-white py-1 px-5">Learn more</button>
+            
+                {/* <h6 className="line3"> We develop corporate & brand strategies that <br className="mobile-no" /> set you apart</h6> */}
 
-                    {/* <h6 className="line3"> We develop corporate & brand strategies that <br className="mobile-no" /> set you apart</h6> */}
-
-
-                </div>
-                
-
-                {/* THE MUTE ICON IS HERE */}
-                <div className="col-3 .bg-danger text-center">
-                <img className=".img-fluid mute2 mute-mobile-yes" src="https://res.cloudinary.com/not-set/image/upload/v1634901154/mute_1_2_joyf7a.png" alt="" />
-                </div>
 
             </div>
+
+
+            {/* THE MUTE ICON IS HERE */}
+            <div className="mute-box text-center .bg-info">
+                <img className=".img-fluid mute-img mute-mobile-yes .bg-danger" src="https://res.cloudinary.com/not-set/image/upload/v1634901154/mute_1_2_joyf7a.png" alt="" />
+            </div>
+
         </div>
 
-    </section>
-    {/* Section-One ends here */}
+
+    </div>
+
+    
+    
 
 
 

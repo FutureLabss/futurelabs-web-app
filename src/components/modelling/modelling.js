320b1a96d46a661ee52e5353187feee3047390a7
Justin Mark <jwmark212@gmail.com>
2022-02-08 04:03:50 -0800

    MODELLING Video uploaded

--- src/components/modelling/modelling.js
+++ src/components/modelling/modelling.js
@@ -17,12 +17,18 @@ return (
 
 
     {/* Section-One begins here */}
-    <section className="section1 modelling-image1">
+  <div className="video-container .text-danger .bg-danger">
+        <video autoPlay muted loop className="">
+            {/* <Transformation endOffset="30" videoCodec="auto" /> */}
+            <source 
+              src="https://res.cloudinary.com/not-set/video/upload/v1644321263/Military_Drone_Animation_by_Maverick_for_FutureLABS_jm6vgp.mp4" type="video/mp4" 
+            /> 
+            
+          
+        </video> 
 
-        <div className="container">
-            <div className="row .bg-warning d-flex justify-space-between">
-                <div className="col-9 .bg-primary px-3 px-sm-1 text-white">
-
+        <div className="parent-box1 d-flex justify-space-between ontop .bg-warning px-5 text-white">
+            <div className="text-box .bg-danger">
                   <h5 className="line1">
                     3D Modeling
                   </h5>
@@ -30,22 +36,22 @@ return (
 
                   <h6 className="line3"> What if a 12 year old genius reimagined the future of <br className="mobile-no" /> urban transportation, flying cars and all?</h6>
 
-
-                </div>
-                
-
-                {/* THE MUTE ICON IS HERE */}
-                <div className="col-3 .bg-danger text-center">
-                  <img className=".img-fluid mute2 mute-mobile-yes" src="https://res.cloudinary.com/not-set/image/upload/v1634901154/mute_1_2_joyf7a.png" alt="" />
-                </div>
-
             </div>
+
+
+            {/* THE MUTE ICON IS HERE */}
+            <div className="mute-box text-center .bg-info">
+                <img className=".img-fluid mute-img mute-mobile-yes .bg-danger" src="https://res.cloudinary.com/not-set/image/upload/v1634901154/mute_1_2_joyf7a.png" alt="" />
+            </div>
+
         </div>
 
-    </section>
-    {/* Section-One ends here */}
+
+    </div>
 
 
+
+    
 
 
     {/* Section-One begins here */}
--- src/components/strategy/strategy.js
+++ src/components/strategy/strategy.js
@@ -51,34 +51,7 @@ return (
 
 
 
-    {/* Section-One begins here */}
-    <section className="section1 strategy-image1">
-
-        <div className="container">
-            <div className="row .bg-warning d-flex justify-space-between">
-                <div className="col-9 .bg-primary px-3 px-sm-1 text-white">
-
-                    <h5 className="line1">
-                        Strategy
-                    </h5>
-                    {/* <button type="button" class="btn btn-outline-warning strategy-button-text mt-4 text-white">LEARN MORE</button> */}
-
-                    <h6 className="line3"> We develop corporate & brand strategies that <br className="mobile-no" /> set you apart</h6>
-
-
-                </div>
-                
-
-                {/* THE MUTE ICON IS HERE */}
-                <div className="col-3 .bg-danger text-center">
-                  <img className=".img-fluid mute2 mute-mobile-yes" src="https://res.cloudinary.com/not-set/image/upload/v1634901154/mute_1_2_joyf7a.png" alt="" />
-                </div>
-
-            </div>
-        </div>
-
-    </section>
-    {/* Section-One ends here */}
+    
 
 
 

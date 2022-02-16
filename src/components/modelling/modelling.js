4cb431228c4076f1cf3251d6fd849910abfc5b16
Justin Mark <jwmark212@gmail.com>
2022-02-16 07:05:12 -0800

    Trying the scroll-behaviour

--- src/components/modelling/modelling.js
+++ src/components/modelling/modelling.js
@@ -224,7 +224,7 @@ return (
                     </h5>
                     {/* <button type="button" class="btn btn-outline-warning modelling-button-text mt-4 text-white">LEARN MORE</button> */}
 
-                    <h6 className="line3"> From flat screens, we create bodies <br className="mobile-no" /> that appear to pop out of it. </h6>
+                    <h6 className="line3"> On flat screens, we create bodies <br className="mobile-no" /> that appear to pop out of it. </h6>
 
 
                 </div>

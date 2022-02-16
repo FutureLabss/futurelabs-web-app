d6a3522c371b08c45af2fa19b16bc85a6f883e30
Usenmfon <usenmfonuko@gmail.com>
2022-02-15 21:23:21 -0800

    added assets

---
+++ src/components/assets/index.js
@@ -0,0 +1,12 @@
+import bookmark from './images/bookmark.png'
+import comment from './images/comment.png'
+import favourite from './images/favourite.png'
+import favouriteSVG from './svg/favourite.svg'
+
+
+export const icons = {
+    bookmark,
+    comment,
+    favourite,
+    favouriteSVG,
+}

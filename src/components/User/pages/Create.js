0aeb36f415b7940d207244b033320a4633b4d2b1
Usenmfon <usenmfonuko@gmail.com>
2022-03-04 13:08:59 -0800

    updated code

--- src/components/User/pages/Create.js
+++ src/components/User/pages/Create.js
@@ -1,8 +1,6 @@
 import React, {useState, useEffect}  from "react";
 import { TextField, Button, Grid, Stack, CircularProgress, } from "@mui/material";
-import BackspaceIcon from '@mui/icons-material/Backspace';
 import PublishIcon from '@mui/icons-material/Publish';
-import { Nav } from 'react-bootstrap'
 import { useAuth } from '../../../contexts/AuthContext'
 import { BlogManager } from "../../../services/blog"
 import { useNavigate } from 'react-router-dom'
@@ -23,12 +21,12 @@ export default function Article() {
   const [values, setValues] = useState({});
   const [file, setFile] = useState("")
 
-  let select;
-  if(file){
-    select = <p style={{color: "green"}}>File Selected</p>
-  } else {
-    select = <p style={{color: "red"}}>File Not Selected</p>
-  }
+  // let select;
+  // if(file){
+  //   select = <p style={{color: "green"}}>File Selected</p>
+  // } else {
+  //   select = <p style={{color: "red"}}>File Not Selected</p>
+  // }
 
   // var username;
   useEffect(()=>{
@@ -73,27 +71,23 @@ export default function Article() {
   }
 
   return (
-    <Grid sx={{marginTop: '7rem'}} container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} maxWidth={{}}>
-      <Stack sx={{ maxWidth: '30%'}}>
-        <Nav.Link href="/"><Button variant="contained"  startIcon={<BackspaceIcon />}></Button></Nav.Link>
-      </Stack>
-     
+    <Grid sx={{marginTop: '1rem'}} rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }} maxWidth={{}}>
+        
       <form onSubmit={handleSubmit}>
         <Grid sx={{display: 'flex', flexDirection: 'column', alignItems:'center'}} rowSpacing={1} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
           <Grid item xs={10}>
-            <Stack spacing={2} component="form" sx={{ width: 500, maxWidth: '100%',}} noValidate autoComplete="off">
-              <TextField hiddenLabel placeholder="Title" variant="filled"  size="small" name="title" onChange={handleChange}  />
-              <TextField hiddenLabel id="filled-hidden-label-small" placeholder="Description" name="description" onChange={handleChange} variant="filled" size="small" />
-              <label htmlFor="contained-button-file">
-                <label>Upload a photo
+            <Stack spacing={2} component="form" sx={{ width: 900, maxWidth: '100%',}} noValidate autoComplete="off">
+              <TextField hiddenLabel placeholder="Title" variant="filled"  size="small" name="title" onChange={handleChange} required />
+              <Grid item sx={{display: 'flex', justifyContent: 'space-between'}}>
+                <TextField sx={{ width: 800, maxWidth: '100%',}} hiddenLabel id="filled-hidden-label-small" placeholder="Description" name="description" onChange={handleChange} variant="filled" size="small" required/>
+                <label htmlFor="contained-button-file">
                   <input type="file" onChange={handleFile} required></input>
                 </label>
-                <Stack>{select}</Stack>
-            </label>
+              </Grid>
             </Stack>
           </Grid>
           <Grid item sx={{ marginTop: '5rem'}} xs={10} rowSpacing={2} columnSpacing={{ xs: 1, sm: 1, md: 1 }}>
-              <Stack sx={{ width: '90vw', maxWidth: '100%'}}>
+              <Stack sx={{ width: '90vw', maxWidth: '100%',}}>
                 <Editor 
                   editorState={editorState}
                   onEditorStateChange={handleEditorChange}
@@ -102,16 +96,15 @@ export default function Article() {
                   toolbarClassName="toolbar-class"
                 />
               </Stack>
-              <Stack>
+              <Stack sx={{marginTop: '1rem', display: 'flex', alignItems: 'center'}}>
                 {loading ? <div className="text-center"><CircularProgress size={30} /></div> : 
-                <Button type="submit" variant="contained" startIcon={<PublishIcon />}>
+                <Button sx={{width: 200, backgroundColor: '#ff8a00'}} type="submit" variant="contained" startIcon={<PublishIcon />}>
                   PUBLISH
                 </Button>}
               </Stack>
           </Grid>
         </Grid>
-        </form>
-      
+        </form>      
       </Grid>
   );
 }
